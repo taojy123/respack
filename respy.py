aef34b1b413e0351a7139733102deb1bf222f11e
taojy123 <taojy123@163.com>
2017-09-29 11:44:47 +0800

    improve

--- respy.py
+++ respy.py
@@ -1,45 +1,61 @@
+
 import os
+
 
 # the default resources dir
 RESOURCES_DIR = "C:\\respy_resources\\"
 
 
-def init():
+def init_dir(resources_dir=RESOURCES_DIR):
     """ Make sure RESOURCES_DIR and resources.py exist """
-    if not os.path.exists(RESOURCES_DIR):
-        os.makedirs(RESOURCES_DIR)
+    if not os.path.exists(resources_dir):
+        os.makedirs(resources_dir)
+
+
+def init_resources():
     open("resources.py", "a")
     if not open("resources.py").read():
-        cls_res()
+        open("resources.py", "w").write("res_dict={}")
 
-def make_res(resorce_file):
+
+def set(resorce_file, target_name=None):
     """
-    make the resource file into resources.py
+    set the resource file into resources.py
     the resorce_file should be a absolute path or a relative path
     """
-    init()
-    filename = resorce_file.split("\\")[-1]
+    if not os.path.exists(resorce_file):
+        print(resorce_file + ' not found!')
+        return
+    if not target_name:
+        target_name = os.path.split(resorce_file)[-1]
+    init_resources()
     bin_str = open(resorce_file, "rb").read()
     read_str = open("resources.py").read()
-    out_str = read_str[:-1] + repr(filename) + ":" + repr(bin_str) + ",\n}"
+    out_str = read_str[:-1] + repr(target_name) + ":" + repr(bin_str) + ",\n}"
     open("resources.py", "w").write(out_str)
 
 
-def release_res():
+def release(resources_dir=RESOURCES_DIR):
     """ release the files in resources.py to resources dir (C:\respy_resources\) """
-    init()
-    from resources import res_dict
-    for filename, bin_str in res_dict.iteritems():
-        open(get_res(filename), "wb").write(bin_str)
+    try:
+        from resources import res_dict
+    except ImportError as e:
+        print('resources module not found!')
+        res_dict = {}
+
+    init_dir(resources_dir)
+    for target_name, bin_str in res_dict.iteritems():
+        open(get(target_name, resources_dir, False), "wb").write(bin_str)
 
     
-def get_res(filename):
-    """ get resource file path in resources dir (C:\respy_resources\) """
-    return RESOURCES_DIR + filename
+def get(target_name, resources_dir=RESOURCES_DIR, flag=True):
+    """ get target file path in resources dir (C:\respy_resources\) """
+    if flag:
+        release(resources_dir)
+    path = os.path.join(resources_dir, target_name)
+    if not os.path.exists(path) and flag:
+        return '<not_exists>'
+    return path
 
 
-def cls_res():
-    """ init the resources.py """
-    open("resources.py", "w").write("res_dict={}")
 
-init()
